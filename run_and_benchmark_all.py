0f03fa89c987595486b4c67b0c12dc93fca0092a
Brandon Anzaldi <banzaldi@fapl.co>
2018-12-03 23:31:28 -0800

    Make number of timeit runs configurable

--- run_and_benchmark_all.py
+++ run_and_benchmark_all.py
@@ -4,6 +4,8 @@ from pathlib import Path
 modules = sorted(list(Path('.').glob('day*')), key=lambda x: int(str(x)[3:]))
 modules = [str(x) for x in modules]
 
+run_number = 250
+
 for module in modules:
     print('Testing %s...' % module)
     print('Testing part_1()...')
@@ -11,14 +13,14 @@ for module in modules:
 from {module}.{module} import part_1
 part_1()
 '''.format(module=module)
-    part_1_bench = timeit(exec_str, number=100)
+    part_1_bench = timeit(exec_str, number=run_number)
     print('Total execution time: %1.5f\nAverage execution time: %1.5f' %
-          (part_1_bench, part_1_bench / 100,))
+          (part_1_bench, part_1_bench / run_number,))
     print('Testing part_2()...')
     exec_str = '''
 from {module}.{module} import part_2
 part_2()
 '''.format(module=module)
-    part_2_bench = timeit(exec_str, number=100)
+    part_2_bench = timeit(exec_str, number=run_number)
     print('Total execution time: %1.5f\nAverage execution time: %1.5f' %
-          (part_2_bench, part_2_bench / 100,))
+          (part_2_bench, part_2_bench / run_number,))
